b1b8e4858a3021bb4ee69d2472b40baf3c855d17
sansanty03 <138645645+sansanty03@users.noreply.github.com>
2025-02-23 16:40:19 -0700

    Update Transacciones.java

--- TicketWizard/src/main/java/ticketwizard/entidades/Transacciones.java
+++ TicketWizard/src/main/java/ticketwizard/entidades/Transacciones.java
@@ -4,10 +4,76 @@
  */
 package ticketwizard.entidades;
 
+import java.util.Date;
+import java.util.Objects;
+
 /**
  *
  * @author santi
  */
 public class Transacciones {
+    Integer codigoTransaccion;
+    Date fehcaHora;
+    String tipoAdquision;
+    float costo;
+    Integer codigoComprador;
+    Integer codigoBoleto;
+    Integer codigoVendedor;
+
+    public Integer getCodigoTransaccion() {
+        return codigoTransaccion;
+    }
+
+    public Date getFehcaHora() {
+        return fehcaHora;
+    }
+
+    public String getTipoAdquision() {
+        return tipoAdquision;
+    }
+
+    public float getCosto() {
+        return costo;
+    }
+
+    public Integer getCodigoComprador() {
+        return codigoComprador;
+    }
+
+    public Integer getCodigoBoleto() {
+        return codigoBoleto;
+    }
+
+    public Integer getCodigoVendedor() {
+        return codigoVendedor;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.codigoTransaccion);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Transacciones other = (Transacciones) obj;
+        return Objects.equals(this.codigoTransaccion, other.codigoTransaccion);
+    }
+
+    @Override
+    public String toString() {
+        return "Transacciones{" + "codigoTransaccion=" + codigoTransaccion + ", fehcaHora=" + fehcaHora + ", tipoAdquision=" + tipoAdquision + ", costo=" + costo + ", codigoComprador=" + codigoComprador + ", codigoBoleto=" + codigoBoleto + ", codigoVendedor=" + codigoVendedor + '}';
+    }
+    
     
 }
